a925295e04cc526cb771a29f35816347ff9b10d1
Somesh Kumar Pal <palsomesh101@gmail.com>
2023-09-18 09:15:49 +0530

    Add Scientific Operations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { useReducer } from 'react';
+import { useReducer, useEffect } from 'react';
 import './App.scss';
 import DigitButton from './DigitButton'
 import Operation from './Operation';
@@ -18,6 +18,7 @@ function reducer(state, { type, payload }) {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
       if (state.overwrite) {
+        
         return {
           ...state,
           currentOperand: payload.digit,
@@ -27,8 +28,15 @@ function reducer(state, { type, payload }) {
       if (payload.digit === "0" && state.currentOperand === "0") {
         return state
       }
-      if (payload.digit === "." && state.currentOperand.includes(".")) {
-        return state
+      
+      if (payload.digit === "." && (state.currentOperand === null )) {
+        if(state.currentOperand=== null){
+          return  {
+            ...state,
+            currentOperand: payload.digit,
+            
+          }
+        }else return state
       }
 
       return {
@@ -36,6 +44,7 @@ function reducer(state, { type, payload }) {
         currentOperand: `${state.currentOperand || ""}${payload.digit}`,
       }
     case ACTIONS.CHOOSE_OPERATION:
+      
       if (state.currentOperand == null && state.previousOperand == null) {
         return state
       }
@@ -49,14 +58,15 @@ function reducer(state, { type, payload }) {
         payload.operation = "/"
       }else if(payload.operation === "fa-solid fa-angle-up"){
         payload.operation ="^"
-      }
+      }else if(payload.operation === "fa-sharp fa-solid fa-percent")
+        payload.operation="%"
       if (state.scientificOperation != null) {
         state.currentOperand = scientificEvaluate(state);
         state.scientificOperation = null;
         return {
           ...state,
           previousOperand: evaluate(state),
-          currentOperand: null,
+          currentOperand: 0,
           operation: payload.operation,
           scientificOperation: null,
         }
@@ -74,20 +84,43 @@ function reducer(state, { type, payload }) {
           ...state,
           operation: payload.operation,
           previousOperand: state.currentOperand,
-          currentOperand: null,
+          currentOperand: 0,
         }
       }
       return {
         ...state,
         previousOperand: evaluate(state),
         operation: payload.operation,
-        currentOperand: null,
+        currentOperand: 0,
       }
     case ACTIONS.CHOOSE_SCIENTIFIC_OPERATION:
+      if(payload.operation === "fa-sharp fa-solid fa-percent")
+        payload.operation="%"
+      if(state.currentOperand == null && state.previousOperand == null && state.operation == null)
+      return {
+        ...state,
+        scientificOperation: payload.operation,
+      }
       if (state.operation == null) {
-        return state;
+        return {
+          ...state,
+          overwrite: true,
+          scientificOperation: payload.operation,
+          operation:"*",
+          previousOperand: state.currentOperand,
+          currentOperand: 0,
+        }
       }
       if (state.operation != null) {
+        if(state.currentOperand != null && state.previousOperand!=null)
+        return{
+          ...state,
+          overwrite: true,
+          previousOperand: evaluate(state),
+          operation: "*",
+          currentOperand: 0,
+          scientificOperation: payload.operation,
+        }
         return {
           ...state,
           scientificOperation: payload.operation,
@@ -103,19 +136,65 @@ function reducer(state, { type, payload }) {
         return {
           ...state,
           overwrite: false,
-          currentOperand: null,
+          currentOperand: 0,
+        };
+      }
+      
+      if (
+        (state.currentOperand === 0 || state.currentOperand == null) &&
+        state.previousOperand == null
+      ) {
+        return {
+          ...state,
+          currentOperand: 0,
+        };
+      }
+      
+      if (typeof state.currentOperand === 'string' && state.currentOperand.length === 1) {
+        return { ...state, currentOperand: 0 };
+      }
+      
+      if (typeof state.currentOperand === 'string' && state.currentOperand !== '0') {
+        return {
+          ...state,
+          currentOperand: state.currentOperand.slice(0, -1),
+        };
+      } else if (state.scientificOperation != null) {
+        console.log(1);
+        return {
+          ...state,
+          scientificOperation: null,
+        };
+      } else if (state.operation != null) {
+        return {
+          ...state,
+          operation: null,
+        };
+      } else if (typeof state.previousOperand === 'string') {
+        return {
+          ...state,
+          previousOperand: state.previousOperand.slice(0, -1),
+        };
+      } else {
+        return {
+          ...state,
+          currentOperand: state.currentOperand.slice(0, -1),
+        };
+      }
+      
+    case ACTIONS.EVALUATE:
+      if (state.scientificOperation != null) {
+        state.currentOperand = scientificEvaluate(state);
+        state.scientificOperation = null;
+        // if(state.operation !== null)
+        return {
+          ...state,
+          previousOperand: evaluate(state),
+          currentOperand: 0,
+          operation: null,
+          scientificOperation: null,
         }
       }
-      if (state.currentOperand == null) return state
-      if (state.currentOperand.length === 1) {
-        return { ...state, currentOperand: null }
-      }
-
-      return {
-        ...state,
-        currentOperand: state.currentOperand.slice(0, -1),
-      }
-    case ACTIONS.EVALUATE:
       if (
         state.operation == null ||
         state.currentOperand == null ||
@@ -123,7 +202,7 @@ function reducer(state, { type, payload }) {
       ) {
         return state
       }
-
+      
       return {
         ...state,
         overwrite: true,
@@ -164,9 +243,6 @@ function scientificEvaluate({ currentOperand, previousOperand, operation, scient
     case "e":
       computation = Math.E * current;
       break
-    case "%":
-      computation = 0.01 * current;
-      break
     case "!":
       function factorial(n) {
         if (n === 0 || n === 1) {
@@ -191,7 +267,6 @@ function scientificEvaluate({ currentOperand, previousOperand, operation, scient
 function evaluate({ currentOperand, previousOperand, operation }) {
   const prev = parseFloat(previousOperand)
   const current = parseFloat(currentOperand)
-  console.log(previousOperand);
   if (isNaN(prev) || isNaN(current)) return ""
   let computation = ""
   switch (operation) {
@@ -210,6 +285,9 @@ function evaluate({ currentOperand, previousOperand, operation }) {
       case "^":
         computation= Math.pow(prev, current);
         break
+      case "%":
+        computation = prev * 0.01 * current;
+        break
     default:
       computation = current
   }
@@ -220,10 +298,17 @@ const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
   maximumFractionDigits: 0,
 })
 function formatOperand(operand) {
-  if (operand == null) return
-  const [integer, decimal] = operand.split(".")
-  if (decimal == null) return INTEGER_FORMATTER.format(integer)
-  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
+  if (typeof operand !== 'string' || operand === null) {
+    return ''; // or some default value that you want to return
+  }
+
+  const [integer, decimal] = operand.split(".");
+  
+  if (decimal === undefined) {
+    return INTEGER_FORMATTER.format(integer);
+  }
+  
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`;
 }
 
 function App() {
@@ -232,28 +317,54 @@ function App() {
     reducer,
     {}
   )
+  const handleKeyPress = (e) => {
+    const {key}=e;
+    
+    const digit  = key;
+    const operation = key;
+    if (/[0-9.]/.test(digit)) {
+      // If the key is a number, dispatch NUMBER action
+      
+      dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })
+    } else if (/[+\-*/]/.test(digit)) {
+      // If the key is an operation, dispatch OPERATION action
+      dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: {operation} });
+    } else if (digit === 'Enter') {
+      // If the key is Enter, dispatch EVALUATE action
+      dispatch({ type: ACTIONS.EVALUATE })
+    }
+    // Handle other cases as needed
+  };
+  useEffect(() => {
+    document.addEventListener('keypress', handleKeyPress);
 
+    // Remove event listener when the component unmounts
+    return () => {
+      document.removeEventListener('keypress', handleKeyPress);
+    };
+  }, []);
   return (
     <div className="flex background">
-      <div className='left'></div>
+      <div className='left'>
+        
+        <div className='center'> <h1 className='center-text'>Scientific Calculator</h1>
+        <span>by Somesh Kumar Pal (versin 2.0) </span></div>
+      </div>
       <div className='right'>
         <div className='flex-direction-column calculator'>
           <div className="flex-direction-column input-box">
             <div className="ContentChat solution"></div>
             <div className='flex-column previous-operant'><span>{formatOperand(previousOperand)} {operation} {scientificOperation}</span></div>
             <div className='current-operant'>{formatOperand(currentOperand)}</div>
-            {/* <div className="input-field">
-          <input id="display" type="text" key="name" required readonly />
-          <label for="name">0</label>
-        </div> */}
+            
           </div>
           <div className="flex-wrap-space-evenly inputs top-sci">
-            <div className="cal-btn operator sci chat">Exp</div>
+            {/* <div className="cal-btn operator sci chat">Exp</div> */}
             <ScientificOperation operation="sin" cl="sci" dispatch={dispatch} />
             <ScientificOperation operation="cos" cl="sci" dispatch={dispatch} />
             <ScientificOperation operation="tan" cl="sci" dispatch={dispatch} />
             <ScientificOperation operation="log" cl="sci" dispatch={dispatch} />
-
+            <ScientificOperation operation="ln" cl="sci" dispatch={dispatch} />
           </div>
           <div className="flex cal-btns">
             <div className="flex-wrap-space-evenly inputs">
@@ -264,7 +375,7 @@ function App() {
                 <i className="fa-sharp fa-solid fa-delete-left"></i>
               </div>
 
-              <Operation operation="fa-sharp fa-solid fa-percent" cl="operator" dispatch={dispatch} />
+              <Operation operation="fa-sharp fa-solid fa-percent" cl=" operator" dispatch={dispatch} />
 
 
               <Operation operation="fa-sharp fa-solid fa-divide" cl="operator" dispatch={dispatch} />
@@ -289,17 +400,17 @@ function App() {
               <div className="cal-btn equal" onClick={() => dispatch({ type: ACTIONS.EVALUATE })}><i className=" fa-sharp fa-solid fa-equals"></i></div>
             </div>
             <div className="flex-wrap-space-evenly sci-btns">
-              <ScientificOperation operation="ln" cl="sci" dispatch={dispatch} />
+              
               <Operation operation="fa-solid fa-angle-up" cl="sci" dispatch={dispatch} />
               <ScientificOperation operation="!" cl="sci" dispatch={dispatch} />
               <ScientificOperation operation="π" cl="sci" dispatch={dispatch} />
               <ScientificOperation operation="e" cl="sci" dispatch={dispatch} />
               <ScientificOperation operation="√" cl="sci" dispatch={dispatch} />
-              <ScientificOperation operation=")" cl="sci" dispatch={dispatch} />
             </div>
           </div>
         </div>
       </div>
+      {/* <div className='right'></div> */}
     </div>
   );
 }
--- src/ScientificOperation.jsx
+++ src/ScientificOperation.jsx
@@ -1,7 +1,15 @@
 import { ACTIONS } from "./App"
 
 export default function ScientificOperation({ dispatch, operation, cl }) {
-  return (
+  if(operation ==="fa-sharp fa-solid fa-percent")
+  return (  <div className={`cal-btn ${cl}`}
+  onClick={() => dispatch({ type: ACTIONS.CHOOSE_SCIENTIFIC_OPERATION, payload: { operation } })}
+  >
+    
+    <i className={` ${operation}`}></i>
+  </div>
+)
+  else return (
     <div className={`${cl}`}
     onClick={() => dispatch({ type: ACTIONS.CHOOSE_SCIENTIFIC_OPERATION, payload: { operation } })}
     >
